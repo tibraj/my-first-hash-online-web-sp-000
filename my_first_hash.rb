98b5e9219168ea5aa4e517b1a948fd738013c032
Taulant Ibraj <tibraj94@gmail.com>
2020-04-01 15:44:07 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,5 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+  fav_albums = {"Burial" => "Untrue", "Gas" => "Pop", "Bjork" => "Vespertine"}
 end
 
 
