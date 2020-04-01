4f67ce034ebf154e3acae1501d33e8ffa0248c5f
Taulant Ibraj <tibraj94@gmail.com>
2020-04-01 15:51:15 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -23,14 +23,7 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
   shipping_manifest["muskets", "gun powder"] = 2, 4
   shipping_manifest["gun powder"] = 4
   shipping_manifest
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
-end
+  end
